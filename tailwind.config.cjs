a1d6d5448014294f832c5445376ce763c840bea2
diegonicita <diegonicita@gmail.com>
2023-06-28 23:16:31 -0300

    Add tailwind and daisyui. Create components and mytheme

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#570df8',
+          secondary: '#f000b8',
+          accent: '#1dcdbc',
+          neutral: '#2b3440',
+          'base-100': '#ffffff',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+}
