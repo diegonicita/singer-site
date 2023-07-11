f8f56c7e78ec49152aea1a4813fe35c7a91bbe55
diegonicita <diegonicita@gmail.com>
2023-07-11 15:22:37 -0300

    Add Themes to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
+      // applyBaseStyles: false,
     }),
   ],
 })
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#570df8',
-          secondary: '#f000b8',
-          accent: '#1dcdbc',
-          neutral: '#2b3440',
-          'base-100': '#ffffff',
-          info: '#3abff8',
-          success: '#36d399',
-          warning: '#fbbd23',
-          error: '#f87272',
-        },
-      },
-    ],
-  },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
 }
